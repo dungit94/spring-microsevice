ad5d74136f013d7b6f556261b182570c9d192676
rikkei_dungnq <dungnq@rikkeisoft.com>
2019-09-12 17:34:09 +0700

    first commit

---
+++ loyalty-pointbank/src/main/java/com/viettel/myparking/loyaltypointbank/LoyaltyPointbankApplication.java
@@ -0,0 +1,13 @@
+package com.viettel.myparking.loyaltypointbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyPointbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyPointbankApplication.class, args);
+	}
+
+}
---
+++ loyalty-rule-engine/src/main/java/com/viettel/myparking/loyaltyruleengine/LoyaltyRuleEngineApplication.java
@@ -0,0 +1,13 @@
+package com.viettel.myparking.loyaltyruleengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyRuleEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyRuleEngineApplication.class, args);
+	}
+
+}
