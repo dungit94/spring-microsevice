b334dab9998c776f8a57e28efe9ea130a623ee4e
rikkei_dungnq <dungnq@rikkeisoft.com>
2019-09-12 17:31:11 +0700

    first commit

---
+++ loyalty-campain/src/main/java/com/viettel/myparking/loyaltycampain/LoyaltyCampainApplication.java
@@ -0,0 +1,13 @@
+package com.viettel.myparking.loyaltycampain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyCampainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyCampainApplication.class, args);
+	}
+
+}
