a486a1c0fe8c1a9f0a8d63b7ccdf4e1214298a02
rikkei_dungnq <dungnq@rikkeisoft.com>
2019-09-16 11:25:25 +0700

    commit

--- loyalty-rule-engine/src/main/java/com/viettel/myparking/loyaltyruleengine/LoyaltyRuleEngineApplication.java
+++ loyalty-rule-engine/src/main/java/com/viettel/myparking/loyaltyruleengine/LoyaltyRuleEngineApplication.java
@@ -2,12 +2,13 @@ package com.viettel.myparking.loyaltyruleengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LoyaltyRuleEngineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoyaltyRuleEngineApplication.class, args);
 	}
-
 }
